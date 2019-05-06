4165e2a1601cd1158a87e7fbd37098651b0f86a4
Srujan Bethi <ssbethi@gmail.com>
2019-05-06 12:23:14 -0600

    Added event listeners, setup html, working on implementing game rules

---
+++ app.js
@@ -0,0 +1,52 @@
+
+var sq1 = document.getElementById("sq1");
+var sq2 = document.getElementById("sq2");
+var sq3 = document.getElementById("sq3");
+var sq4 = document.getElementById("sq4");
+var sq5 = document.getElementById("sq5");
+var sq6 = document.getElementById("sq6");
+var sq7 = document.getElementById("sq7");
+var sq8 = document.getElementById("sq8");
+var sq9 = document.getElementById("sq9");
+
+
+var addXtoSq1 = () => {
+	sq1.innerHTML = "X";
+}
+var addXtoSq2 = () => {
+	sq2.innerHTML = "X";
+}
+var addXtoSq3 = () => {
+	sq3.innerHTML = "X";
+}
+var addXtoSq4 = () => {
+	sq4.innerHTML = "X";
+}
+var addXtoSq5 = () => {
+	sq5.innerHTML = "X";
+}
+var addXtoSq6 = () => {
+	sq6.innerHTML = "X";
+}
+var addXtoSq7 = () => {
+	sq7.innerHTML = "X";
+}
+var addXtoSq8 = () => {
+	sq8.innerHTML = "X";
+}
+var addXtoSq9 = () => {
+	sq9.innerHTML = "X";
+}
+
+
+sq1.addEventListener("click", addXtoSq1)
+sq2.addEventListener("click", addXtoSq2)
+sq3.addEventListener("click", addXtoSq3)
+sq4.addEventListener("click", addXtoSq4)
+sq5.addEventListener("click", addXtoSq5)
+sq6.addEventListener("click", addXtoSq6)
+sq7.addEventListener("click", addXtoSq7)
+sq8.addEventListener("click", addXtoSq8)
+sq9.addEventListener("click", addXtoSq9)
+
+
